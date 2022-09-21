66bf17ac36867ea84311144900a5f56ed502e5a5
Paladin-from-335 <andrei.anufriev.work@gmail.com>
2022-09-21 15:14:54 +0300

    refactoring (RemindService interface)

--- src/main/java/com/example/remindbot/service/RemindService.java
+++ src/main/java/com/example/remindbot/service/RemindService.java
@@ -1,34 +1,13 @@
 package com.example.remindbot.service;
 
-import com.example.remindbot.model.Reminder;
-import com.example.remindbot.repo.RemindRepo;
-import java.sql.Timestamp;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
+import com.example.remindbot.model.entity.Reminder;
+import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-@PropertySource(value = "classpath:scheduler.properties")
-public class RemindService {
+public interface RemindService {
 
-    private final RemindRepo remindRepo;
+    void saveRemind(Reminder reminder);
 
-    public void saveRemind(Reminder reminder) {
-        remindRepo.save(reminder);
-    }
+    void deleteOld();
 
-    @Scheduled(cron = "${cron.delete-old}")
-    public void deleteOld() {
-        this.remindRepo.deleteOld(Timestamp.valueOf(LocalDateTime.now(ZoneId.of("GMT"))));
-    }
-
-    @Scheduled(cron = "${cron.get-actual}")
-    public void getActual() {
-        this.remindRepo.actualReminders(LocalDate.now(ZoneId.of("GMT")));
-    }
+    List<Reminder> getActual();
 }
